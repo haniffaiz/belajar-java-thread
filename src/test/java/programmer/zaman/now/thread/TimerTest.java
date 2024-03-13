044f4989c45a78a1d2b6534a3bae1c2a3358e91f
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-13 22:58:21 +0700

    Timer. delay, periodic

---
+++ src/test/java/programmer/zaman/now/thread/TimerTest.java
@@ -0,0 +1,41 @@
+package programmer.zaman.now.thread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+//gunanya ketika membuat task yang delay atau periodic
+public class TimerTest {
+
+
+    @Test
+    void delayedJob() throws InterruptedException {
+        var task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Delayed Job");
+            }
+        };
+
+        var timer = new Timer();
+        timer.schedule(task,2000);
+
+        Thread.sleep(3000L);
+    }
+
+    @Test
+    void periodicJob() throws InterruptedException {
+        var task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Delayed Job");
+            }
+        };
+
+        var timer = new Timer();
+        timer.schedule(task,2000,2000);
+
+        Thread.sleep(10_000L);
+    }
+}
